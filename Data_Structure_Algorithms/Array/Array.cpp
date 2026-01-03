c06bc0d8700e1d617a574886bc3be6bcc059138c
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-03 21:34:51 +0530

    3 SUM

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -32,3 +32,38 @@ public:
         return {-1, -1};
     }
 };
+
+// 3 Question -- 3Sum
+class Solution {
+public:
+vector<vector<int>> ans;
+    void   twoSum(vector<int>& nums, int i, int j, int t){
+        while(i <j){
+            int sum= nums[i]+nums[j];
+            if(sum < t) i++;
+            else if(sum > t) j--;
+            else {
+                while(i<j && nums[i] == nums[i+1]) i++;
+                while(i< j && nums[j] == nums[j-1]) j--;
+                ans.push_back({-t, nums[i], nums[j]});
+                i++; j--;
+            }
+        }
+        
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        if(n <3) return {} ;
+        sort(nums.begin(), end(nums));
+        
+        for(int i= 0; i<n; i++){
+            if(i> 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int k= nums[i];
+            int target = -k;
+            twoSum(nums, i+1, n-1, target);
+        }
+        return  ans;
+    }
+};
