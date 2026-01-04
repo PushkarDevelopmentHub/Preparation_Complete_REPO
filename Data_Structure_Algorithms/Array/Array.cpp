e850e2993fa4afc6940ad4f145da51599293f689
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-04 18:51:54 +0530

    // 11 Question -- Group Anagrams | using sorting Leetcode 49

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -274,3 +274,59 @@ public:
 
     }
 };
+
+// 11 Question -- Group Anagrams | using sorting Leetcode 49
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> mp;
+        for(int i=0; i<n; i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+
+        for(auto it : mp){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
+
+//Group Anagrams | without sortingGroup Anagrams | without sorting
+class Solution {
+public:
+string generate(string& word){
+    int a[26] = {0};
+    for(char &ch : word){
+        a[ch - 'a']++;
+    }
+    string NW = "";
+    for(int i=0; i<26; i++){
+        int freq = a[i];
+        if(freq > 0){
+            NW += string(freq, i+'a');
+        }
+    }
+    return NW;
+}
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> mp;
+        for(int i=0; i<n; i++){
+            string word = strs[i];
+            
+            string nw = generate(word);
+
+            mp[nw].push_back(strs[i]);
+        }
+
+        for(auto it : mp){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
