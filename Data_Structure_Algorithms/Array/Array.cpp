16875ddd884f5d02bb32b1e1d76517297bdfe202
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-04 17:24:10 +0530

    // 7 Question -- Next Permutation

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -156,3 +156,31 @@ public:
         return arr;
     }
 };
+
+// 7 Question -- Maximum Product Subarray 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // next_permutation(begin(nums), end(nums));
+
+        //2apporach
+        int n = nums.size();
+        int gola_index =-1;
+        for(int i= n-2; i>=0; i--){
+            if(nums[i]< nums[i+1]){
+                gola_index = i;
+                break;
+            }
+        }
+        if(gola_index != -1){
+            for(int i = n-1; i>=gola_index; i--){
+                if(nums[i] > nums[gola_index]){
+                    swap(nums[i], nums[gola_index]);
+                    break;
+                }
+            }
+        }
+        reverse(nums.begin() + gola_index +1, nums.end());
+
+    }
+};
