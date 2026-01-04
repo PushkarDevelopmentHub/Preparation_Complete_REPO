d716b05383d43b65b48be820324271dfe558d903
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-05 00:22:21 +0530

    13 Question -- Diagonal Traverse | Leetcode 498 | Similar to Leetcode 1329

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -361,3 +361,34 @@ public:
     }
 };
 
+
+// 13 Question -- Diagonal Traverse | Leetcode 498 | Similar to Leetcode 1329
+// mp[i+j] approach
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+         int m = mat.size(); //row
+        int n= mat[0].size(); //col
+        vector<int> result;
+
+        map<int, vector<int>> mp;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                mp[i+ j].push_back(mat[i][j]);
+            }
+        }
+
+        bool flip = true;
+        for(auto &it : mp){
+            if(flip){
+                reverse(it.second.begin(), it.second.end());
+            }
+            for(int &num : it.second){
+                result.push_back(num);
+            }
+            flip = !flip;
+        }
+        return result;
+        
+    }
+};
