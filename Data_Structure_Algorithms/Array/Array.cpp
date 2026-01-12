00f9a36df52f3fb7e04538b2dc09f558f44572ff
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-12 23:23:17 +0530

    14 Question -- Sort Colors

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -394,3 +394,25 @@ public:
 };
 
 
+// 14 Question -- Sort Colors
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+int i=0, j=0, k=n-1;
+while(j<=k){
+    if(nums[j]==0){
+        swap(nums[i], nums[j]);
+        i++;
+        j++;
+    }
+    else if(nums[j]==1){
+        j++;
+    }
+    else{
+        swap(nums[j], nums[k]);
+        k--;
+    }
+}
+    }
+}; 
