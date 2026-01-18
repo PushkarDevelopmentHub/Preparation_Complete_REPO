19aec289425bf8f52dbeefdb7885880aea7a8fd4
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-18 22:38:06 +0530

    Question - 22 -- Set Mismatch | Leetcode 645

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -570,3 +570,26 @@ public:
     return false;
     }
 };
+
+// Question - 22 -- Set Mismatch | Leetcode 645
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n= nums.size();
+        int dup= -1;
+        int missing = -1;
+        for(int i =0; i<n; i++){
+            if(nums[abs(nums[i]) - 1] <0){
+                dup = abs(nums[i]);
+            }else{
+                nums[abs(nums[i]) - 1] *= (-1);
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i] > 0){
+                missing = (i+1);
+            }
+        }
+        return {dup, missing};
+    }
+};
