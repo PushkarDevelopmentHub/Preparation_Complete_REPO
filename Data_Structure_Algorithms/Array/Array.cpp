b7e162cd77765cf5be7eaa10aa1883d5352abd83
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-18 19:36:59 +0530

    Question - 20 -- Largest Perimeter Triangle | Leetcode 976

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -535,3 +535,19 @@ public:
           return false;
     }
 };
+
+// Question - 20 -- Largest Perimeter Triangle | Leetcode 976
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), end(nums));
+
+        for(int i= n-3; i>=0; i--){
+            if(nums[i]  + nums[i+1] > nums[i+2]){
+                return nums[i]  + nums[i+1] + nums[i+2];
+            }
+        }
+        return 0;
+    }
+};
