6911a0d5147b2a9412f3f0e1a2da3deb5b15188c
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-18 18:58:46 +0530

    Question 18 - Find Pivot Index | Leetcode 724

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -491,3 +491,26 @@ public:
     return res;
     }
 };
+
+
+// Question 18 - Find Pivot Index | Leetcode 724
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int sum =0;
+        for(int &x : nums){
+            sum += x;
+        }
+        int cs = 0;
+        for(int i=0; i<n; i++){
+            int ls = cs;
+            int rs = sum - cs -nums[i];
+            if(ls == rs){
+                return i;
+            }
+            cs += nums[i];
+        }
+        return -1;
+    }
+};
