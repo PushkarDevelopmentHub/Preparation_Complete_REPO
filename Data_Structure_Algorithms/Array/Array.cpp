82c0915a1cbe73baeb937f1b067faf47e94ffb02
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-01-21 08:04:24 +0530

    Question - 24 -- Image Overlap | Leetcode 835

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -617,3 +617,40 @@ public:
         return false;
     }
 };
+
+// Question - 24 -- Image Overlap | Leetcode 835
+class Solution {
+public:
+    int countoverlaps(vector<vector<int>>& img1, vector<vector<int>>& img2, int rowoff, int coloff){
+        int n = img1.size();
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                // A[i][j] -> B[i+rowoff][j+coloff]
+                int B_i = i+rowoff;
+                int B_j= j+ coloff;
+
+                if(B_i < 0 || B_i >= n || B_j < 0 || B_j >= n){
+                    continue;
+                }
+                if(img1[i][j] == 1 && img2[B_i][B_j] == 1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int m = img2.size();
+        //m*n row= n , col =n;
+        int maxoverlap = 0;
+        for(int rowoff = -n+1; rowoff<n; rowoff++){
+            for(int coloff = -n+1; coloff < n; coloff++){
+                int count = countoverlaps(img1, img2, rowoff, coloff);
+                maxoverlap = max(maxoverlap, count);
+            }
+        }
+        return maxoverlap;
+    }
+};
