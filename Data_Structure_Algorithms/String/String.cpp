64b5bbb3265fe7b49412ce332d0fa9db042853e2
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-02-09 23:22:31 +0530

    Leetcode 1662 -- Check If Two String Arrays are Equivalent

--- Data_Structure_Algorithms/String/String.cpp
+++ Data_Structure_Algorithms/String/String.cpp
@@ -116,3 +116,65 @@ public:
         return res;
     }
 };
+
+
+
+//Approach-1 (Simple concatenation)
+//T.C : O(n*k) - n and m = length of word1 and word2 respectively
+//S.C : O(n+k)
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1 = "";
+        string s2 = "";
+        
+        for(string s:word1)
+            s1 += s;
+        
+        for(string s:word2)
+            s2 += s;
+        
+        return s1==s2;
+    }
+};
+
+
+// Leetcode 1662 -- Check If Two String Arrays are Equivalent
+//Approach-2 (Using comparison character by character using index)
+//T.C : O(n*k) - n and m = length of word1 and word2 respectively
+//S.C : O(1)
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        int w1i = 0; int i = 0;
+        int w2i = 0; int j = 0;
+        
+        
+        while(w1i < m && w2i < n) {
+            if(word1[w1i][i] != word2[w2i][j])
+                return false;
+            
+            i++;
+            j++;
+            
+            if(i == word1[w1i].length()) {
+                i = 0;
+                w1i++;
+            }
+            
+            if(j == word2[w2i].length()) {
+                j = 0;
+                w2i++;
+            }
+        }
+        
+        if(w1i == word1.size() && w2i == word2.size())
+            return true;
+        
+        return false;
+        
+    }
+};
