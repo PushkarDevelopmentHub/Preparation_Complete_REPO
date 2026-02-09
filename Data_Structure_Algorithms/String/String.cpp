14a4363078ef9d8946ad9ac8fd1d8d7e6b247c1c
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-02-09 23:02:57 +0530

    Leetcode 12 -- Integer to Roman

--- Data_Structure_Algorithms/String/String.cpp
+++ Data_Structure_Algorithms/String/String.cpp
@@ -92,3 +92,27 @@ countAndSay(4) = RLE of "21" = "1211" ek barr 2 likha hai 1 bar ek likah hai
     return result;
     }
 };
+
+
+
+// Leetcode 12 -- Integer to Roman
+class Solution {
+public:
+//      vector<int> val{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+//      vector<string> sym{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+  
+    string intToRoman(int num) {
+         static vector<int> val{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  static  vector<string> sym{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string res = "";
+        for(int i=0; i<13; i++){
+            if(num == 0) break;
+            int times = num/val[i];
+            while(times--){
+                res += sym[i];
+            }
+            num = num%val[i];
+        }
+        return res;
+    }
+};
