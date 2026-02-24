fcbf3add046b3ab769128fa270b2c2dae591d294
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-02-24 07:51:12 +0530

    Leetcode 1022 -- Sum of Root To Leaf Binary Numbers

--- Data_Structure_Algorithms/EveryDay_Leetcode_Question/Leetcode_question.cpp
+++ Data_Structure_Algorithms/EveryDay_Leetcode_Question/Leetcode_question.cpp
@@ -2735,3 +2735,36 @@ public:
     return false;
     }
 };   
+
+
+
+// Leetcode 1022 -- Sum of Root To Leaf Binary Numbers | Leetcode 1022
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int solve(TreeNode* root, int val){
+        if(!root){
+            return 0;
+        }
+    val = (2*val) + root->val;
+    if(root->left == NULL && root->right == NULL){
+        return val;
+    }
+    return solve(root->left, val) + solve(root->right, val);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        //left shift  *2
+        //right shift /2
+        return solve(root, 0);
+    }
+};
