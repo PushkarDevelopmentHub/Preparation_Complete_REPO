bd144b48649dbf7d711ef601cd1ad003268c8b0d
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-03-01 17:06:46 +0530

    Leetcode 1689 -- Partitioning Into Minimum Number Of Deci-Binary Numbers

--- Data_Structure_Algorithms/EveryDay_Leetcode_Question/Leetcode_question.cpp
+++ Data_Structure_Algorithms/EveryDay_Leetcode_Question/Leetcode_question.cpp
@@ -2984,3 +2984,44 @@ public:
     }
 };
 
+
+
+// Leetcode 1689 -- Partitioning Into Minimum Number Of Deci-Binary Numbers | Leetcode 1689
+//Method-1 (This method explains why Method-2 trick worked)
+//T.C : O(n*D), where D is the max digit which can be <= 9 ~= O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minPartitions(string n) {
+        int count = 0;
+
+        while (true) {
+            bool changed = false;
+
+            for (char &ch : n) {
+                if (ch != '0') {
+                    ch--;          // subtract 1
+                    changed = true;
+                }
+            }
+
+            if (!changed)
+                break;
+
+            count++;
+        }
+
+        return count;
+    }
+};
+
+
+//Method-2 (The largest character will take time to reach 0)
+//T.C : o(length)
+//S.C : O(1)
+class Solution {
+public:
+    int minPartitions(string n) {
+        return *max_element(begin(n), end(n))-'0';
+    }
+};
