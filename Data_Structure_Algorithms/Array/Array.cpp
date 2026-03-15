33b3fe61b5c016837e15f4be5b05903b2dec7488
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-03-15 22:54:29 +0530

    Question - 27 -- Where Will the Ball Fall | Leetcode 1706

--- Data_Structure_Algorithms/Array/Array.cpp
+++ Data_Structure_Algorithms/Array/Array.cpp
@@ -727,3 +727,41 @@ public:
             return true;
     }
 };
+
+
+// Question - 27 -- Where Will the Ball Fall | Leetcode 1706
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> res;
+        for(int ball = 0; ball <n; ball++){
+            int row=0; 
+            int col = ball;
+            bool atka = false;
+            while(row<m && col <n){
+                if(grid[row][col] == 1){
+                    if(col == n-1 || grid[row][col+1] == -1){
+                        atka = true;
+                        break;
+                    }
+                    col++;
+                }else{
+                    if(col == 0 || grid[row][col-1] == 1){
+                        atka = true;
+                        break;
+                    }
+                    col--;
+                }
+                row++;
+            }
+            if(atka == true){
+                res.push_back(-1);
+            }else{
+                res.push_back(col);
+            }
+        }
+        return res;
+    }
+};
