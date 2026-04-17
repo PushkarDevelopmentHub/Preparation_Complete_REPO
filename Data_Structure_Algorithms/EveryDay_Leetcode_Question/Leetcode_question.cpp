9b20d47c3129da4104140ffc68496728230559ea
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-04-18 00:08:15 +0530

    Minimum Absolute Distance Between Mirror Pairs | Simplest Explanation | Leetcode 3761

--- Data_Structure_Algorithms/EveryDay_Leetcode_Question/Leetcode_question.cpp
+++ Data_Structure_Algorithms/EveryDay_Leetcode_Question/Leetcode_question.cpp
@@ -5409,3 +5409,47 @@ public:
         return result;
     }
 };
+
+
+
+
+
+//Minimum Absolute Distance Between Mirror Pairs | Simplest Explanation | Leetcode 3761
+//Approach - Store reverses in map - iterate and find
+//T.C : O(n * log(10(num)))
+//S.C : O(n)
+class Solution {
+public:
+    int getReverse(int n) {
+        int rev = 0;
+
+        while(n > 0) {
+            int rem = n%10;
+
+            rev = rev*10 + rem;
+
+            n /= 10;
+        }
+
+        return rev;
+    }
+    int minMirrorPairDistance(vector<int>& nums) {
+        int n = nums.size();
+
+        unordered_map<int, int> mp; //reversed -> idx
+
+        int result = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            if(mp.count(nums[i])) {
+                result = min(result, i - mp[nums[i]]);
+            }
+
+            mp[getReverse(nums[i])] = i;
+        }
+
+        return result == INT_MAX ? -1 : result;
+    }
+};
+
+
