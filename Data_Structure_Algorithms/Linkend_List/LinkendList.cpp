ee3b9a78b604e37f4de981e7e1d54f9e72bf4d33
GoodGuyPJ <workwithgoodguypj@gmail.com>
2026-06-23 08:02:54 +0530

    1. Sort a LinkedList of 0's, 1's and 2's 2. Remove Nth Node from the end of the LinkedList

--- Data_Structure_Algorithms/Linkend_List/LinkendList.cpp
+++ Data_Structure_Algorithms/Linkend_List/LinkendList.cpp
@@ -83,4 +83,60 @@ Node* oddEvenList(Node* head){
 TC-> O(n) SC-> O(1)
 
 
-//
+//L7. Sort a LinkedList of 0's, 1's and 2's | Multiple Approaches
+Node* SortList(Node* head){
+  if(head == NULL || head->next == NULL){
+    return head;
+  }
+  Node* zeroHead = new Node(-1);
+  Node* zeroTail = zeroHead;
+  Node* oneHead = new Node(-1);
+  Node* oneTail = one Head;
+  Node* twoHead = new Node(-1); 
+  Node* twoTail = twoHead;
+
+  Node* temp = head;
+  while(temp != NULL){
+    if(temp->data == 0){
+      zeroTail->next = temp;
+      zeroTail = temp;
+    }
+    else if(temp->data == 1){
+      oneTail->next = temp;
+      oneTail = temp;
+    }
+    else{
+      towTail->next = temp;
+      twoTail = temp;
+    }
+    temp = temp->next;
+  }
+
+  zeroTail=>next = (oneHead->next) ? oneHead->next : twoHead->next;
+  Node* newHead = zeroHead->next;
+  delete zeroHead;
+  delete oneHead;
+  delete twoHead;
+  return newHead;
+}
+TC-> O(N) SC-> O(1)
+
+//L8. Remove Nth Node from the end of the LinkedList | Multiple Approaches
+Node* removeKthNode(Node* head, int k){
+  if(head == NULL || head->next == NULL){
+    return head;
+  }
+  Node* fast = head;
+  Node* slow = head;
+  for(int i=0; i<k; i++){
+    fast = fast->next;
+  }
+  while(fast->next != NULL){
+    fast = fast->next;
+    slow = slow->next;
+  }
+  Node* delNode = slow->next;
+  slow->next = slow->next->next;
+  delete delNode;
+  return head;
+}
